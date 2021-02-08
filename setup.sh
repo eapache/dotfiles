81071f6c9ded2347ad302fd3e90b4d6094286425
Evan Huus <eapache@gmail.com>
2021-02-08 11:14:19 -0500

    Try again

--- setup.sh
+++ setup.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
+for FILE in gitconfig #zshrc
+do
+    cat ~/dotfiles/$FILE >> ~/.$FILE
+done
+
+for FILE in gitignore vimrc
+do
+    cat ~/dotfiles/$FILE >> ~/.$FILE
+done
+
+mkdir -p ~/.vim/autoload
+ln -s ~/dotfiles/plug.vim ~/.vim/autoload/plug.vim
+
+vim -es -i NONE -c "PlugInstall" -c "q"
+
 exit 0
